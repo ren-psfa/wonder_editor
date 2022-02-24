0bff4c4b1ceede1ce13a6e2bd0e96fabb7eae0fb
ren <0110ynwa@gmail.com>
2022-02-24 22:17:28 +0900

    各model にvalidates を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,4 +21,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -21,4 +21,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :article
+  validates :article_id, uniqueness: { scope: :user_id }
 end
